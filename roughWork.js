8bdea57674179609ab6791509f1ca9b246cce047
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-22 15:10:54 +0530

    Advance Javascript

---
+++ roughWork.js
@@ -0,0 +1,10 @@
+var list =[1,2,3,4];
+var list1=[...list,5,6,7]
+
+//console.log(list1)
+
+
+var list2 =[1,2,3,4,5,6,7];
+list2.splice(3,2);//here 3 is in index from where 2 items have to remove;
+
+console.log(list2)
