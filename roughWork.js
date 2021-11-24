a81621884689c2ef8aa122214631b480f11c04d6
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-24 15:54:21 +0530

    API fetch .then .catch
    fetch(url) => fetch is a function=> create promice bcz it takes time
        https://reqres.in/
        to summerize, using the fetch api will look like this
        fetch(url)
        .then(function(){
    
        });
        .catch(function(){
    
        });
    to collect the fetched  data use .JSON

--- roughWork.js
+++ roughWork.js
@@ -8,3 +8,10 @@ var list2 =[1,2,3,4,5,6,7];
 list2.splice(3,2);//here 3 is in index from where 2 items have to remove;
 
 console.log(list2)
+
+
+
+var str = [1,2,3,4]
+    var data = str.data;
+
+    console.log("str: ",str) //ans=>str: [ 1, 2, 3, 4 ]
