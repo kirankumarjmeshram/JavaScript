6c18a677c8735a1ba634e675dae56ab677ca4136
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-30 14:31:39 +0530

    arrow functions  cannot access this. property
    arrow functions are lightweight functions cannot access this. property

---
+++ ES6/arrowApplication.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4];
+
+arr.forEach((el)=>{
+    console.log("el :",el);
+})
--- ES6/arrowFunction.js
+++ ES6/arrowFunction.js
@@ -1,3 +1,4 @@
+
 function test(){
     console.log('Hello World');
 }
@@ -10,3 +11,28 @@ let test = function () {
 let test = ()=>{
     console.log('Hello World');
 }
+//both are same
+// double =(x)=>{
+//     return x*2
+// }
+
+// function double(x){
+//     return x*2
+// }
+
+
+
+//arrow functions are lightweight functions
+//cannot acces this. property
+
+
+// var myobj ={
+//     i:10;
+//     b:()=>console.log(this.i,this),//arrow functions
+//     c:function(){
+//         console.log(this.i,this);
+//     }//normal function
+// }
+
+// myobj.b();//prints undefined, Window{...} (or the global object)
+// myobj.c();//prints q0,Object{....}
