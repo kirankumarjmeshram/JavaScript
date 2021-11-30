b06e868e9d599997a4fe274cf29102c8f35fee4e
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-30 14:12:44 +0530

    ES6 Arrow function

---
+++ ES6/arrowFunction.js
@@ -0,0 +1,12 @@
+function test(){
+    console.log('Hello World');
+}
+
+let test = function () {
+    console.log('Hello World');
+}
+
+//arrow function
+let test = ()=>{
+    console.log('Hello World');
+}
