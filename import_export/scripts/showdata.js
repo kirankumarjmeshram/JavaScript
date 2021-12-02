072b5d625f0965e34140b0c4262753cad8b03e23
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-02 16:30:06 +0530

    appending data with apis

---
+++ import_export/scripts/showdata.js
@@ -0,0 +1,37 @@
+//logical reusibility
+
+//1. getData (fetch,url) => get the data
+//2. appendData (data,location) => append data in location
+
+//electronics,jewellery (url will vary)
+async function getData(url){
+
+    try{
+        let res = await fetch(url)
+
+        let data = await res.json();
+        return data
+        }
+
+
+        catch(err){
+            console.log("err: ",err)
+        }
+    }
+
+function appendData(data, location){
+    data.forEach((el)=>{
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        let img = document.createElement("img");
+
+        img.src = el.image;
+        p.innerText = el.title;
+        div.append(img,p);
+        location.append(div)
+    });
+}
+
+//object=>{}
+//u can also export var num obj etc
+export {getData,appendData}
