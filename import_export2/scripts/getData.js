6b8edef84caf0153712f5dd71dd4281e2631b30b
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-03 16:34:37 +0530

    img change nd get repo added

--- import_export2/scripts/getData.js
+++ import_export2/scripts/getData.js
@@ -3,7 +3,18 @@
 //2. api call to github
 
 
-async function(){
-    res = await fetch(url);
-    data =await res.json()
+async function getData(user){
+
+    try{
+        let response = await fetch(`https://api.github.com/${user}`);
+        let  data =await response.json()
+        return data;
+    
+
+    }
+    catch(err){
+console.log("err: ",err)
+    }
 }
+
+export default getData
